90cc4267077397970090dfa9bf9cfceb502ec6e1
cike44 <nixiaoqin44@163.com>
2017-11-20 15:53:36 +0800

    GTIT的udp版本

--- ipOverDtn/GTIT/main.c
+++ ipOverDtn/GTIT/main.c
@@ -4,6 +4,10 @@ int serialFd;
 int sockfd;
 int reset_tcp = 1;
 
+struct sockaddr_in addr;
+
+struct sockaddr_in serverGet;
+
 extern int readnum;
 
 
@@ -11,6 +15,8 @@ char g_m908_dev_path[32]	 = {};
 char g_tcp_host[32]	 = {};//IP
 int  g_tcp_port	= 0;
 int g_baud = 115200;
+
+
 
 int main(int argc, char *argv[]){
    
@@ -46,7 +52,16 @@ int main(int argc, char *argv[]){
    g_baud = atoi(argv[2]);
    strcpy(g_tcp_host, argv[3]);
    g_tcp_port = atoi(argv[4]);
+
+
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons (g_tcp_port);
+   addr.sin_addr.s_addr = inet_addr(g_tcp_host);
    
+   serverGet.sin_family=AF_INET;  
+   serverGet.sin_port=htons(7089);  
+   serverGet.sin_addr.s_addr=INADDR_ANY;
+
    printf("m908 device path=%s\n", g_m908_dev_path);
    printf("m908 device baud=%d\n", g_baud);
    printf("transfrom host=%s\n",g_tcp_host);
@@ -92,11 +107,11 @@ int main(int argc, char *argv[]){
         {
             printf("Send data to server... ...\n");
 
-            int sendnum = send (sockfd,&read_buff,length, 0);
+            int sendnum = sendto (sockfd,&read_buff,length, 0, (struct sockaddr *)&addr, sizeof(addr));
             printf("send data to server length:%d \n",sendnum);
             if ( sendnum == -1 ||sendnum == 0)
             {
-                perror ("Send");
+                perror ("Sendto");
                 close(sockfd);
             }
             printf("Send data to server end !!! \n");
--- ipOverDtn/GTIT/tcpclicent.c
+++ ipOverDtn/GTIT/tcpclicent.c
@@ -13,6 +13,10 @@ extern int reset_tcp;
 
  extern  char g_tcp_host[]	;
  extern  int  g_tcp_port;
+
+ extern struct sockaddr_in addr;
+ extern struct sockaddr_in serverGet;
+
 //extern config_info config_infomation;
 
 int connectout = 0;
@@ -20,95 +24,48 @@ int connectout = 0;
 unsigned char recv_server_buff[512];
 
 
-int set_tcp_keepAlive(int fd, int start, int interval, int count)  
-{  
-    int keepAlive = 1;  
-    if (fd < 0 || start < 0 || interval < 0 || count < 0) return -1;  
-    //启用心跳机制，如果您想关闭，将keepAlive置零即可  
-    if(setsockopt(fd,SOL_SOCKET,SO_KEEPALIVE,(void*)&keepAlive,sizeof(keepAlive)) == -1)  
-    {  
-        perror("setsockopt");  
-        return -1;  
-    }  
-    //启用心跳机制开始到首次心跳侦测包发送之间的空闲时间  
-    if(setsockopt(fd,SOL_TCP,TCP_KEEPIDLE,(void *)&start,sizeof(start)) == -1)  
-    {  
-        perror("setsockopt");  
-        return -1;  
-    }  
-    //两次心跳侦测包之间的间隔时间  
-    if(setsockopt(fd,SOL_TCP,TCP_KEEPINTVL,(void *)&interval,sizeof(interval)) == -1)  
-    {  
-        perror("setsockopt");  
-        return -1;  
-    }  
-    //探测次数，即将几次探测失败判定为TCP断开  
-    if(setsockopt(fd,SOL_TCP,TCP_KEEPCNT,(void *)&count,sizeof(count)) == -1)  
-    {  
-        perror("setsockopt");  
-        return -1;  
-    }  
-    return 0;  
-}
+
 
 void *starttcp(void *argv)
 {
+    int addr_len=sizeof(addr);  
+    int getlen=sizeof(serverGet);
       
-       while(1)
-       {  
+     //  while(1)
+      // {  
  
-                 while(1)
-                 { 
                  
 	                 printf ("Client: Socket Create... ...\n");
-	                 sockfd = socket (AF_INET, SOCK_STREAM,0);
+	                 sockfd = socket (AF_INET, SOCK_DGRAM,0);
 	                 if (sockfd == -1) 
 	                 {
 		                 perror ("Socket");
 	                 }     
-	                 printf ("Client: Address Connect... ...\n");
-	    
-		             set_tcp_keepAlive(sockfd,2,2,2);
-		
-	                 struct sockaddr_in addr;
-	                 addr.sin_family = AF_INET;
-	                 addr.sin_port = htons (g_tcp_port);
-	                 addr.sin_addr.s_addr = inet_addr(g_tcp_host);
-            
-	                 if ((reset_tcp = connect (sockfd,(struct sockaddr*)&addr,sizeof (addr)))==-1)
-	                 {   
-                                       printf("reset_tcp: %d \n",reset_tcp);
-	                               perror ("connect");   
-                
-	                 }
-                         if(reset_tcp == 0)
-                         {
-                               break;
-                         }
-                         close(sockfd); 
-                      sleep(2);						 
-                 }
+                        bind(sockfd,(struct sockaddr*)&serverGet,getlen);
+	                // printf ("Client: Address Connect... ...\n");
+
+    
 	         printf ("client: send data... ...\n");
 
 	         while(1)
                  {
                              
-                         printf("recv server data... ...\n");
+                       //  printf("recv server data... ...\n");
                          int readnum = 0;
-                         if(reset_tcp == 0)
-                         {
-		                    readnum = read(sockfd,recv_server_buff,sizeof(recv_server_buff));
+                        // if(reset_tcp == 0)
+                        // {
+		                    readnum = recvfrom(sockfd,recv_server_buff,sizeof(recv_server_buff), 0, (struct sockaddr *)&serverGet, (socklen_t*)&getlen);
 		                    printf("readnum: %d \n",readnum);
 		                    if((readnum == -1) || (readnum == 0))
 		                    {
-			                        perror("read");
+			                        perror("recvfrom");
 			                        break;
 		                    }
-                         }
-                         else
-                         {
-                                    break;
-                         }
+                        // }
+                        // else
+                        // {
+                        //            break;
+                        // }
                          printf("recv server data end !!! !!!\n");        
                          printf("send server data to serial ... ...\n");
                          int n = write (serialFd,recv_server_buff,readnum);
@@ -124,9 +81,9 @@ void *starttcp(void *argv)
                                     printf("%hhx ",recv_server_buff[num]);
                          }
                          printf("\n");
-                 }
+              }
         close(sockfd);
-    }
+   // }
 
 }
 
