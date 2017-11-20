cc6ea39b62b7c6bfc3c9181c8e0b1a386e1782d7
cike44 <nixiaoqin44@163.com>
2017-11-20 21:39:47 +0800

    remove

--- ipOverCCSDS/send/step1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo执行 更改参数 
-gnome-terminal -x bash -c "sudo ctsced -f -C . -c config-he-tcp;exec bash;" & 
-sleep 1 
-gnome-terminal -x bash -c "./aos-send 127.0.0.1;exec bash;" & 
-sleep 1 
-gnome-terminal -x bash -c "sudo ./pcap-ip eth0;exec bash;" &
